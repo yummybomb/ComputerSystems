a728fbbc2a76ab4e312b8032a19526213691a3d3
Nicholas Chen <nickchen12345@gmail.com>
2023-10-16 11:29:16 -0400

    made mymalloc function with helpers

--- HW1/mymalloc.c
+++ HW1/mymalloc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MEMSIZE 512
 #define heap ((char *)memory)
@@ -8,21 +9,86 @@
 static double memory[MEMSIZE];
 
 
+int GetChunkSize(void* start);
+bool IsFree(void* start);
+void SetChunkSize(void* start, int size);
+void MarkAsAllocated(void* start);
+bool NextChunkIsUninitialized(void* start);
+void SetNextChunkSize(void* start, int size);
+void* GetNextChunk(void* start);
+
 void *mymalloc(size_t _Size) {
   if (_Size = 0){
+    printf("Error: cannot allocate 0 bytes\n");
     return NULL;
   }
 
-  int size = ROUNDUP8(size);
+  int size = ROUNDUP8(_Size);
 
   char* res = NULL;
   char* memStart = heap;
   char* memEnd = memStart + MEMSIZE * sizeof(double);
-  
 
+  while (memStart<memEnd){
+    int chunkSize = GetChunkSize(memStart);
+    bool isFree = IsFree(memStart);
+
+    if (chunkSize == 0 && isFree == false){
+      SetChunkSize(memStart, size + 8);
+      MarkAsAllocated(memStart);
+      res = memStart + 8;
+      isFree = true;
+      SetNextChunkSize(memStart, memEnd - (memStart + size + 8));
+      return res;
+    }
+
+    if (isFree == false && chunkSize >= size + 8){
+      SetChunkSize(memStart, size + 8);
+      MarkAsAllocated(memStart);
+      res = memStart + 8;
+      if (NextChunkIsUninitialized(memStart)){
+                SetNextChunkSize(memStart, chunkSize - (size + 8));
+      }
+
+      return res;
+    }
+    if (isFree == true || chunkSize < size + 8){
+      memStart = GetNextChunk(memStart);
+    }
+  }
+  printf("Error: not enough memory\n");
+  return NULL;
 }
 
 void *myfree(void *_Memory) {}
+
+int GetChunkSize(void* start) {
+    return *(int*)start;
+}
+
+bool IsFree(void* start) {
+    return *((char*)start + 4) == 0;
+}
+
+void SetChunkSize(void* start, int size) {
+    *(int*)start = size;
+}
+
+void MarkAsAllocated(void* start) {
+    *((unsigned char*)start + 4) = 1;
+}
+
+bool NextChunkIsUninitialized(void* start) {
+    return GetChunkSize((unsigned char*)start + GetChunkSize(start)) == 0;
+}
+
+void SetNextChunkSize(void* start, int size) {
+    SetChunkSize((unsigned char*)start + GetChunkSize(start), size);
+}
+
+void* GetNextChunk(void* start) {
+    return (char*)start + GetChunkSize(start);
+}
 
 int main() {
   
