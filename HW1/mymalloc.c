4bbc8d6f2e07beaa397d2c9bb858577e5108a439
Nicholas Chen <nickchen12345@gmail.com>
2023-10-16 11:10:29 -0400

    Changed malloc

--- HW1/mymalloc.c
+++ HW1/mymalloc.c
@@ -1,10 +1,30 @@
-#include "mymalloc.h"
 #include <stdio.h>
 
-int main() {
-  header_t curr = { isUsed: 0, size: MEMSIZE-sizeof(header_t), next : NULL };
+#define MEMSIZE 512
+#define heap ((char *)memory)
+
+#define ROUNDUP8(x) (((x) + 7) & (-8))
+
+static double memory[MEMSIZE];
+
+
+void *mymalloc(size_t _Size) {
+  if (_Size = 0){
+    return NULL;
+  }
+
+  int size = ROUNDUP8(size);
+
+  char* res = NULL;
+  char* memStart = heap;
+  char* memEnd = memStart + MEMSIZE * sizeof(double);
+  
+
 }
 
-void *mymalloc(size_t _Size) {}
-
 void *myfree(void *_Memory) {}
+
+int main() {
+  
+  
+}
