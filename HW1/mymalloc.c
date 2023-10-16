6bf21bfd3d84df2f35d354a91c901c1c18d57b81
Lint Action <lint-action@samuelmeuli.com>
2023-10-16 15:30:53 +0000

    Fix code style issues with clang_format

--- HW1/mymalloc.c
+++ HW1/mymalloc.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define MEMSIZE 512
 #define heap ((char *)memory)
@@ -8,32 +8,31 @@
 
 static double memory[MEMSIZE];
 
-
-int GetChunkSize(void* start);
-bool IsFree(void* start);
-void SetChunkSize(void* start, int size);
-void MarkAsAllocated(void* start);
-bool NextChunkIsUninitialized(void* start);
-void SetNextChunkSize(void* start, int size);
-void* GetNextChunk(void* start);
+int GetChunkSize(void *start);
+bool IsFree(void *start);
+void SetChunkSize(void *start, int size);
+void MarkAsAllocated(void *start);
+bool NextChunkIsUninitialized(void *start);
+void SetNextChunkSize(void *start, int size);
+void *GetNextChunk(void *start);
 
 void *mymalloc(size_t _Size) {
-  if (_Size = 0){
+  if (_Size = 0) {
     printf("Error: cannot allocate 0 bytes\n");
     return NULL;
   }
 
   int size = ROUNDUP8(_Size);
 
-  char* res = NULL;
-  char* memStart = heap;
-  char* memEnd = memStart + MEMSIZE * sizeof(double);
+  char *res = NULL;
+  char *memStart = heap;
+  char *memEnd = memStart + MEMSIZE * sizeof(double);
 
-  while (memStart<memEnd){
+  while (memStart < memEnd) {
     int chunkSize = GetChunkSize(memStart);
     bool isFree = IsFree(memStart);
 
-    if (chunkSize == 0 && isFree == false){
+    if (chunkSize == 0 && isFree == false) {
       SetChunkSize(memStart, size + 8);
       MarkAsAllocated(memStart);
       res = memStart + 8;
@@ -42,17 +41,17 @@ void *mymalloc(size_t _Size) {
       return res;
     }
 
-    if (isFree == false && chunkSize >= size + 8){
+    if (isFree == false && chunkSize >= size + 8) {
       SetChunkSize(memStart, size + 8);
       MarkAsAllocated(memStart);
       res = memStart + 8;
-      if (NextChunkIsUninitialized(memStart)){
-                SetNextChunkSize(memStart, chunkSize - (size + 8));
+      if (NextChunkIsUninitialized(memStart)) {
+        SetNextChunkSize(memStart, chunkSize - (size + 8));
       }
 
       return res;
     }
-    if (isFree == true || chunkSize < size + 8){
+    if (isFree == true || chunkSize < size + 8) {
       memStart = GetNextChunk(memStart);
     }
   }
@@ -62,35 +61,22 @@ void *mymalloc(size_t _Size) {
 
 void *myfree(void *_Memory) {}
 
-int GetChunkSize(void* start) {
-    return *(int*)start;
+int GetChunkSize(void *start) { return *(int *)start; }
+
+bool IsFree(void *start) { return *((char *)start + 4) == 0; }
+
+void SetChunkSize(void *start, int size) { *(int *)start = size; }
+
+void MarkAsAllocated(void *start) { *((unsigned char *)start + 4) = 1; }
+
+bool NextChunkIsUninitialized(void *start) {
+  return GetChunkSize((unsigned char *)start + GetChunkSize(start)) == 0;
 }
 
-bool IsFree(void* start) {
-    return *((char*)start + 4) == 0;
+void SetNextChunkSize(void *start, int size) {
+  SetChunkSize((unsigned char *)start + GetChunkSize(start), size);
 }
 
-void SetChunkSize(void* start, int size) {
-    *(int*)start = size;
-}
+void *GetNextChunk(void *start) { return (char *)start + GetChunkSize(start); }
 
-void MarkAsAllocated(void* start) {
-    *((unsigned char*)start + 4) = 1;
-}
-
-bool NextChunkIsUninitialized(void* start) {
-    return GetChunkSize((unsigned char*)start + GetChunkSize(start)) == 0;
-}
-
-void SetNextChunkSize(void* start, int size) {
-    SetChunkSize((unsigned char*)start + GetChunkSize(start), size);
-}
-
-void* GetNextChunk(void* start) {
-    return (char*)start + GetChunkSize(start);
-}
-
-int main() {
-  
-  
-}
+int main() {}
