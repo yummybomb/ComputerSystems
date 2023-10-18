8f16eb4dffff0e5086fddef5f392a10908e4a08f
Lint Action <lint-action@samuelmeuli.com>
2023-10-18 04:31:57 +0000

    Fix code style issues with clang_format

--- HW1/mymalloc.c
+++ HW1/mymalloc.c
@@ -115,9 +115,7 @@ bool memCleared() {
   return false;
 }
 
-void *GetHeapStart(){
-  return heap;
-}
+void *GetHeapStart() { return heap; }
 
 void CoalesceNextChunk(void *ptr) {
 
--- HW1/mymalloc.h
+++ HW1/mymalloc.h
@@ -1,14 +1,10 @@
 #ifndef MYMALLOC_H
 #define MYMALLOC_H
 
-
-
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
 
-
 #define ROUNDUP8(x) (((x) + 7) & (-8))
-
 
 void *mymalloc(size_t _Size, char *file, int line);
 void myfree(void *_Memory, char *file, int line);
