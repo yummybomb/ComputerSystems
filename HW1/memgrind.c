4504173e10fe43d48c21dd394581b3da80bdf87d
Anuraag Sarkar <anuraag.sarkar@rutgers.edu>
2023-10-20 19:15:14 -0400

    Made Test 4

--- HW1/memgrind.c
+++ HW1/memgrind.c
@@ -63,8 +63,24 @@ void test3() {
    printf("MemClear?: %d\n", memCleared());  // Check if memory is cleared
 }
 
+void test4(){
+   char* charArr[60];
+   int* intArr[60];
+   for(int i = 0; i < 120; i++){
+      if(i % 2 == 0){charArr[i/2] = malloc(sizeof(char*));}
+      else{intArr[i/2] = malloc(sizeof(int*));}
+   }
+   for(int i = 0; i < 120; i++){
+      if(i%2 == 0){free(intArr[i/2]);}
+      else{free(charArr[i/2]);}
+   }
+   printf("TEST 4\n");
+   printf("MemClear?: %d\n", memCleared());  // Check if memory is cleared
+}
+
 int main() {
    test1();
    test2();
    test3();
+   test4();
 }
