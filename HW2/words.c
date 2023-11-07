a7341f8200b9e03e8097caf5c4fc1fbb0a502628
Anuraag <anuraag.sarkar@rutgers.edu>
2023-11-06 19:23:41 -0500

    starting my method

--- HW2/words.c
+++ HW2/words.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <stdbool.h>
 #include <fcntl.h> 
 #include <ctype.h>
 
-bool processFile(char* fileName);
-bool processDirectory(char* fileName);
+#define FILE_NOT_FOUND_ERROR "File not found"
+#define FILE_NOT_OPENED_ERROR "File not opened yet"
+#define STARTSIZE 8
+
+bool processFile(const char* fileName);
+bool processDirectory(const char* dirName);
 
 
 int main(int argc, char* argv[]){
@@ -15,41 +20,80 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    //test first file for now
-    printf("%d\n", processFile(argv[1]));
-    
+    //TESTING FOR NOW
     for (int i = 1; i < argc; i++){
-        break;
+        processFile(argv[i]);
     }
     return 0;
 }
 
-bool processFile(char* fileName) {
+bool processFile(const char* fileName) {
     int fd = open(fileName, O_RDONLY);
     if (fd < 0) {
-        perror("open");
+        fprintf(stderr, "Error on line %d : %s\n", __LINE__, FILE_NOT_FOUND_ERROR);
         return false;
     }
-
-    //max word limit, looking to see how to change this
-    char word[4096];
+    
+    //Initialize buffer
+    char* word = malloc(STARTSIZE);
+    int wordCapacity = STARTSIZE;
     int wordIndex = 0;
-    char curr, prev;
+    
+    char firstThree[3];
+    int bytesRead = read(fd, firstThree, 3);
 
-    //Loop to EOF by character
-    while (read(fd, &curr, 1) > 0) {
-        //logic to find words
-        prev = curr;
+    if(bytesRead == 1){
+        //TODO: Add Only the first character to hashmap
+    }
+    else if(bytesRead == 2){
+        //TODO: Check both characters. This can be done manually
+
+    }
+    else{
+        char c = firstThree[1]; char prev = firstThree[0]; char next = firstThree[2];
+        while (read(fd, &c, 1) > 0){
+            if(wordIndex >= wordCapacity){
+                wordCapacity *= 2;
+                word = realloc(word, wordCapacity);
+            }
+
+
+        }
     }
 
+    bool isValidCharacter()
+
+    /*
+    while (read(fd, &c, 1) > 0) {
+        if (wordIndex >= wordCapacity) {
+                wordCapacity *= 2;
+                word = realloc(word, wordCapacity);
+        }
+
+        if (isValidCharacter(c, prev_c, prev_prev_c)) {            
+            word[wordIndex++] = c;
+        } else if (wordIndex > 0) {
+            word[wordIndex] = '\0';
+            // ADD WORD TO HASHMAPHERE
+
+            word = realloc(word, STARTSIZE);
+            wordCapacity = STARTSIZE;
+            wordIndex = 0;
+        }
+        prev_prev_c = prev_c;
+        prev_c = c;
+    }*/
+
+    free(word);
+
     if (close(fd) < 0) { 
-        perror("close"); 
+        fprintf(stderr, "Error on line %d : %s\n", __LINE__, FILE_NOT_OPENED_ERROR);
         return false;
     }
     return true;
 }
 
-bool processDirectory(char* directory){
+bool processDirectory(const char* dirName){
     //TODO LATER
     return false;
 }
