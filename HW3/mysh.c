23824f312a517effd77fe2a10cd1a97f03efaccd
Nicholas Chen <nickchen12345@gmail.com>
2023-11-18 14:03:06 -0500

    Starter code for mysh batch mode

---
+++ HW3/mysh.c
@@ -0,0 +1,115 @@
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INITIAL_BUFFER_SIZE 256
+// Function prototypes
+int batch_mode(char* fileName);
+void interactive_mode(void);
+// Helper Funcs
+void read_file(FILE* file);
+bool is_empty_or_whitespace(const char* str);
+void process_line(const char* line);
+void handle_error(const char* msg);
+
+int main(int argc, char* argv[]){
+    if (argc > 2) { 
+        fprintf(stderr, "Error: mysh only takes 0 or 1 arguments\n");
+        return 1;
+    } 
+    if (argc == 2) { // Batch mode - 1 argument
+        batch_mode(argv[1]);
+    } 
+    if (argc == 1) { // Interactive mode - 0 args 
+        interactive_mode(); 
+    }
+    return 0;
+}
+
+int batch_mode(char* fileName) {
+    FILE* file = fopen(fileName, "r");
+    if (file == NULL) {
+        handle_error("Error opening file");
+    }
+
+    read_file(file);
+
+    if (fclose(file) != 0) {
+        handle_error("Error closing file");
+    }
+
+    return 0;
+}
+
+void interactive_mode(void) {
+    return;
+}
+
+//Helper Funcs
+void read_file(FILE* file) {
+    // Allocate initial buffer
+    char* line = malloc(INITIAL_BUFFER_SIZE);
+    if (line == NULL) {
+        handle_error("Error allocating memory");
+    }
+
+    size_t bufferSize = INITIAL_BUFFER_SIZE;
+    size_t contentSize = 0;
+
+    // Read character
+    int c;
+    while ((c = getc(file)) != EOF) {
+        if (contentSize >= bufferSize) {
+            bufferSize *= 2;
+            line = realloc(line, bufferSize);
+            if (line == NULL) {
+                handle_error("Error reallocating memory");
+            }
+        }
+
+        // add a character to buffer
+        line[contentSize++] = (char)c;
+
+        // EOL - process and reset buffer
+        if (c == '\n') {
+            line[contentSize] = '\0';
+            process_line(line);
+            contentSize = 0;
+        }
+    }
+
+    // Process leftover content in a line
+    if (contentSize > 0) {
+        line[contentSize] = '\0';
+        process_line(line);
+    }
+
+    // Clean up
+    free(line);
+}
+
+void process_line(const char* line) {
+    if (is_empty_or_whitespace(line)) {
+        return;
+    }
+    //WIP Process Line Here
+    printf("%s", line);
+}
+
+void handle_error(const char* msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+bool is_empty_or_whitespace(const char* str) {
+    while (*str) {
+        if (!isspace((unsigned char)*str))
+            return false;  // Found a non-whitespace character
+        str++;
+    }
+    return true;  // String is empty or contains only whitespace
+}
